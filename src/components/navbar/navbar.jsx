e0d973049ddf03cfd8831c20046516176bca061c
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-02-21 15:09:23 -0600

    Intento de dropdown

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,17 +1,36 @@
+import React, { useState } from "react";
 import "./styles/navbar.css";
-import { loguot } from "@/utils/user";
 
-export default function NavBar({ token, onLogout }) {
+const NavBar = ({ token, onLogout }) => {
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
   const handleLogout = () => {
-    onLogout(loguot(token));
+    setDropdownVisible(false);
+    onLogout(token);
+  };
+
+  const handleProfile = () => {
+    console.log("hola");
+  };
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
   };
 
   return (
     <nav className="navbar">
       <h3>Red social de prueba</h3>
       <div className="navbarDiv">
-        <button onClick={handleLogout}>Cerrar sesión</button>
+        <button onClick={toggleDropdown}>Menú</button>
+        {dropdownVisible && (
+          <div className="dropdown">
+            <button onClick={handleProfile}>Perfil</button>
+            <button onClick={handleLogout}>Cerrar sesión</button>
+          </div>
+        )}
       </div>
     </nav>
   );
-}
+};
+
+export default NavBar;
